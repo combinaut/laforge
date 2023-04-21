d8fc999c9b8e29a48525e8a6b230c1bbd79bc38d
clouie87 <carol.louie@gmail.com>
2023-04-21 11:40:42 -0700

    Add table_names for the laforge models
    We want to ensure that the tables are prefixed by `la_forge`

--- lib/laforge/data_entry.rb
+++ lib/laforge/data_entry.rb
@@ -11,6 +11,8 @@
 
 module LaForge
   class DataEntry < ActiveRecord::Base
+    self.table_name = "la_forge_data_entries"
+
     belongs_to :record, polymorphic: true
     belongs_to :source, class_name: 'LaForge::DataSource'
 
--- lib/laforge/data_source.rb
+++ lib/laforge/data_source.rb
@@ -1,5 +1,7 @@
 module LaForge
   class DataSource < ActiveRecord::Base
+    self.table_name = "la_forge_data_sources"
+
     has_many :data_entries, class_name: 'LaForge::DataEntry'
 
     validates_presence_of :priority
